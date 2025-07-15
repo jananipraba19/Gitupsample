19c85f61003c8e46bc5def1294fd54345c0bf294
jananipraba19 <mail2jana@gmail.com>
2025-07-15 16:48:10 +0530

    classes added

---
+++ src/main/java/com/sample/Gitupsample.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class Gitupsample {
+public void java() {
+	System.out.println("Java");
+	
+}
+public void python() {
+	System.out.println("python");
+	
+}
+public void selenium() {
+	System.out.println("selenium");
+	
+}
+}
---
+++ src/test/java/Git/GitTask.java
@@ -0,0 +1,19 @@
+package Git;
+
+public class GitTask {
+	public void classA() {
+	System.out.println("java");
+}
+	public void ClassB() {
+		System.out.println("Selenium");
+
+	}public void Classc() {
+		System.out.println("TestNG");
+
+	}
+	private void classD() {
+		System.out.println("POM");
+
+	}
+	
+}
